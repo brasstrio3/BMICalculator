103d5c6bbffe76021da74e3325eaf723ca746d0d
Marcus Lundy <mlundy360@gmail.com>
2018-06-07 14:22:41 -0400

    Initial commit

---
+++ app/src/main/java/com/example/administrator/bmicalculator/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.mcandroid.bmicalculator
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setOnClickListenerForButton()
+    }
+
+    private fun calculateBMI():Double{
+        var weight: Double = (numWeight.getText().toString().toDouble())
+        var height: Double = (numHeight.getText().toString().toDouble())
+        return (weight / height / height)
+    }
+
+    private fun setOnClickListenerForButton(){
+        btnCalculateBMI.setOnClickListener{
+            val intent = Intent("com.mcandroid.bmicalculator.BMIResultsScreen")
+            intent.putExtra("BMIResult",calculateBMI())
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/mcandroid/bmicalculator/BMIResultsScreen.kt
@@ -0,0 +1,61 @@
+package com.mcandroid.bmicalculator
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_bmiresults_screen.*
+import java.text.DecimalFormat
+
+class BMIResultsScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bmiresults_screen)
+        showBMIResult()
+        findBMICategory()
+        setExitListener()
+        setCheckAgainListener()
+    }
+
+    fun showBMIResult(){
+        var decFormat = DecimalFormat("#.#")
+        var formattedBMI = decFormat.format(getIntent().getExtras().getDouble("BMIResult"))
+        lblBMIResult.setText(formattedBMI.toString())
+    }
+
+    fun findBMICategory(){
+        var categoryOfBMI = "Unknown"
+        var resultBMI = getIntent().getExtras().getDouble("BMIResult")
+        if(resultBMI < 15){
+            categoryOfBMI = "Very Severely Underweight"
+        } else if(resultBMI in 15..16){
+            categoryOfBMI = "Severely Underweight"
+        } else if(resultBMI > 16 && resultBMI <= 18.5){
+            categoryOfBMI = "Underweight"
+        } else if(resultBMI > 18.5 && resultBMI <= 25){
+            categoryOfBMI = "Normal (Healthy Weight)"
+        } else if(resultBMI in 25..30){
+            categoryOfBMI = "Overweight"
+        } else if(resultBMI in 30..35){
+            categoryOfBMI = "Moderately Obese"
+        } else if(resultBMI in 35..40){
+            categoryOfBMI = "Severely Obese"
+        } else if(resultBMI >= 40){
+            categoryOfBMI = "Very Severely Obese"
+        }
+        lblBMIResultCategory.setText(categoryOfBMI)
+    }
+
+    fun setExitListener() {
+        btnCloseResults.setOnClickListener {
+            this.finishAffinity()
+        }
+    }
+
+    fun setCheckAgainListener(){
+        btnCheckAgain.setOnClickListener{
+            val intent = Intent("com.mcandroid.bmicalculator.MainActivity")
+            startActivity(intent)
+        }
+    }
+}
